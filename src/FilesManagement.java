65b15bb665b6d239f7bdd36423351c043c62987f
Anton Santalov <anton.santalov1995@gmail.com>
2024-07-18 11:59:30 +0400

    Code added.

--- src/FilesManagement.java
+++ src/FilesManagement.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class FilesManagement {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FilesManagement {
+    static void createDirectory(String path, StringBuilder log) {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            if (dir.mkdir()) {
+                log.append("Directory '").append(path).append("' created successfully.\n");
+            } else {
+                log.append("Failed to create directory '").append(path).append("'.\n");
+            }
+        } else {
+            log.append("Directory '").append(path).append("' already exists.\n");
+        }
+    }
+
+    static void createFile(String path, StringBuilder log) {
+        File file = new File(path);
+        try {
+            if (file.createNewFile()) {
+                log.append("File '").append(path).append("' created successfully.\n");
+            } else {
+                log.append("Failed to create file '").append(path).append("'.\n");
+            }
+        } catch (IOException e) {
+            log.append("Error creating file '").append(path).append("': ").append(e.getMessage()).append("\n");
+        }
+    }
+
+    static void writeLogToFile(String path, StringBuilder log) {
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write(log.toString());
+        } catch (IOException e) {
+            log.append("Error writing to file '").append(path).append("': ").append(e.getMessage()).append("\n");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StringBuilder log = new StringBuilder();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        FilesManagement.createDirectory("Games", log);
+
+        FilesManagement.createDirectory("Games/src", log);
+        FilesManagement.createDirectory("Games/res", log);
+        FilesManagement.createDirectory("Games/savegames", log);
+        FilesManagement.createDirectory("Games/temp", log);
+
+        FilesManagement.createDirectory("Games/src/main", log);
+        FilesManagement.createDirectory("Games/src/test", log);
+
+        FilesManagement.createFile("Games/src/main/Main.java", log);
+        FilesManagement.createFile("Games/src/main/Utils.java", log);
+
+        FilesManagement.createDirectory("Games/res/drawables", log);
+        FilesManagement.createDirectory("Games/res/vectors", log);
+        FilesManagement.createDirectory("Games/res/icons", log);
+
+        FilesManagement.createFile("Games/temp/temp.txt", log);
+
+        FilesManagement.writeLogToFile("Games/temp/temp.txt", log);
     }
 }
